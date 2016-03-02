789c3858a159c3034d944e05b67abc0703122665
liangjunfeng <liangjunfeng@ipmacro.com>
2016-03-02 16:36:43 +0800

    init commit.

---
+++ app/src/main/java/linkin/com/apkinfo/ApkInfoActivity.java
@@ -0,0 +1,13 @@
+package linkin.com.apkinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApkInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apk_info);
+    }
+}
